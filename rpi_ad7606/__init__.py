b0ce00310555a0e0426b9df52781d6104278697a
bsumlin <30328179+bsumlin@users.noreply.github.com>
2018-04-16 12:12:22 -0500

    ugh

--- rpi_ad7606/__init__.py
+++ rpi_ad7606/__init__.py
@@ -1,2 +1,2 @@
-from rpi_ad7606.rpi_ad7606 import AD7606, transferFunction, ADCreset, ADCread
-from rpi_ad7606._version import __version__
+from rpi_ad7606 import AD7606, transferFunction, ADCreset, ADCread
+from _version import __version__
